7ef2788bfb89428f83cfcd9b247eeedb22fc9238
smgeek <s.melendez.c@gmail.com>
2022-09-26 07:01:55 -0500

    primera_version

---
+++ PROYECTO_ROL4/Controllers/AccesoController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROYECTO_ROL4.Data;
+using PROYECTO_ROL4.Models;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace PROYECTO_ROL4.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()//Permiti simular formulario de inicio de sesión
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Usuario _usuario)
+        {
+            DA_Logica _da_usuario = new DA_Logica();
+            var usuario = _da_usuario.validarUsuario(_usuario.Correo, _usuario.Clave);
+
+            if( usuario != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, usuario.Nombre),
+                    new Claim("Correo", usuario.Correo)
+                };
+                
+
+                foreach (string rol in usuario.Roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, rol));
+                }
+
+                var ClaimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(ClaimsIdentity));
+
+                return RedirectToAction("Index", "Home"); // redirecciona a la vista de home
+            }
+            else
+            {
+                return View();
+            }
+                
+        }
+
+        public async Task<IActionResult> Salir()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Acceso");
+        }
+    }
+}
---
+++ PROYECTO_ROL4/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(option =>
+{
+    option.LoginPath = "/Acceso/Index";//Especificar nuestra pagina de login
+    option.ExpireTimeSpan = TimeSpan.FromMinutes(2); //Expiración
+    option.AccessDeniedPath = "/Home/Privacy";//Cuando deniegue el acceso a que pagina redirige
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Acceso}/{action=Index}/{id?}");
+
+app.Run();
